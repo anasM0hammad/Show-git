37fcc0d861dfe656bc943349485d832ac7e9231e
anasM0hammad <anas.1633.m@gmail.com>
2018-10-15 14:14:59 +0530

    Complete app with fetching image

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const name = document.querySelector("#name") ;
 const repo = document.querySelector("#repo") ;
 const un = document.querySelector("#un") ;
 const url = document.querySelector("#url") ;
-
+const followers = document.querySelector("#followers") ;
+const image = document.querySelector("#image") ;
 
 const client_id = "";
 const client_secret = "";
@@ -23,7 +24,14 @@ const fetchUser = async (user)=> {
 const showData = () => {
 
     fetchUser(input.value).then((result) => {
-    	console.log(result) ;
+    	// image.innerHTML = `<img src="${result.data.avatar_url}" width="90px" id="image" height="90" style="border-radius: 50%;">`;
+    	image.src=result.data.avatar_url;
+    	name.innerHTML = `<span style="font-style: italic; margin-left: 20px; color: white;" id="name"> ${result.data.name}</span>` ;
+    	repo.innerHTML = `<span style="font-style: italic; margin-left: 20px; color: white;" id="repo"> ${result.data.public_repos}</span>` ;
+    	un.innerHTML = `<span style="font-style: italic; margin-left: 20px; color: white;" id="un"> ${result.data.login}</span>` ;
+    	followers.innerHTML = `<span style="font-style: italic; margin-left: 20px; color: white;" id="followers"> ${result.data.followers}</span>` ;
+    url.innerHTML = `<span style="font-style: italic; margin-left: 20px; color: white;" id="url"><a href="${result.data.html_url}">Click Here</a></span></b></h5>`;
+
     }).catch(error => console.log(error))
 };
 
